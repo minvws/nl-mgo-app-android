63536b73bd55b6bf120e943480ca89acf9d317b5
Bart Nijland <bartnijland91@gmail.com>
2024-03-28 15:11:20 +0100

    Create config screens

---
+++ feature/config/src/test/java/nl/rijksoverheid/mgo/feature/config/ConfigOldVersionScreenTest.kt
@@ -0,0 +1,17 @@
+package nl.rijksoverheid.mgo.feature.config
+
+import nl.rijksoverheid.mgo.framework.snapshots.SnapshotTestRule
+import org.junit.Rule
+import org.junit.Test
+
+internal class ConfigOldVersionScreenTest {
+    @get:Rule
+    val snapshotTestRule = SnapshotTestRule()
+
+    @Test
+    fun launchView() {
+        snapshotTestRule.snapshots {
+            ConfigOldVersionScreenPreview()
+        }
+    }
+}

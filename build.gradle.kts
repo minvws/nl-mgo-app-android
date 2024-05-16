f02b9940336f351dc9baa1f79671dfd418689abe
pk27734 <pk27734@users.noreply.github.com>
2024-05-16 15:51:24 +0200

    Jacoco path is back!

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,5 +18,6 @@ sonar {
     property("sonar.organization", "vws") 
     property("sonar.projectKey", "nl-mgo-app-android-private")
     property("sonar.host.url", "https://sonarcloud.io")
+    property("sonar.coverage.jacoco.xmlReportPaths", "*/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
   }
 }

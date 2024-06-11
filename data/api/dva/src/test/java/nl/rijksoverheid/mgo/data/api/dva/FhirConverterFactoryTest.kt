0b0bcd2853b5298499d07f9ed4e2bb6b63e2d228
Bart Nijland <bartnijland91@gmail.com>
2024-06-11 09:38:47 +0200

    Create test for FhirConverterFactory

---
+++ data/api/dva/src/test/java/nl/rijksoverheid/mgo/data/api/dva/FhirConverterFactoryTest.kt
@@ -0,0 +1,36 @@
+package nl.rijksoverheid.mgo.data.api.dva
+
+import ca.uhn.fhir.context.FhirContext
+import nl.rijksoverheid.mgo.framework.test.TEST_OKHTTP_CLIENT
+import nl.rijksoverheid.mgo.framework.test.TestServerRule
+import org.hl7.fhir.dstu3.model.Bundle
+import org.hl7.fhir.dstu3.model.MedicationStatement
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import kotlinx.coroutines.test.runTest
+
+class FhirConverterFactoryTest {
+    @get:Rule
+    val testServerRule = TestServerRule()
+
+    private val context = FhirContext.forDstu3()
+    private val testServer = testServerRule.testServer
+
+    @Test
+    fun `Given response with empty bundle, when fhir converter factory called, return empty list`() =
+        runTest {
+            // Given
+            val bundle = Bundle()
+            val jsonParser = context.newJsonParser()
+            val bundleJson = jsonParser.encodeResourceToString(bundle)
+            testServer.enqueueJson(bundleJson)
+
+            // When
+            val dvaApi = createDvaApi(okHttpClient = TEST_OKHTTP_CLIENT, baseUrl = testServer.url())
+            val medicationStatements = dvaApi.medicationStatement()
+
+            // Then
+            assertEquals(listOf<MedicationStatement>(), medicationStatements)
+        }
+}

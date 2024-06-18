291a0f79afaa417f6341383869eb81dee15d70c8
Bart Nijland <bartnijland91@gmail.com>
2024-06-18 13:50:35 +0200

    Make status bar same color as background

--- component/theme/src/main/java/nl/rijksoverheid/mgo/component/theme/Theme.kt
+++ component/theme/src/main/java/nl/rijksoverheid/mgo/component/theme/Theme.kt
@@ -1,13 +1,19 @@
 package nl.rijksoverheid.mgo.component.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Typography
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 @Composable
 fun MgoTheme(
@@ -15,12 +21,23 @@ fun MgoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit,
 ) {
+    // Make sure the status bar is the same color as the background
+    val view = LocalView.current
+    val backgroundColor = MaterialTheme.colors.backgroundPrimary().toArgb()
+    if (!view.isInEditMode) {
+        SideEffect {
+            val activity = view.context as Activity
+            activity.window.statusBarColor = backgroundColor
+            WindowCompat.getInsetsController(activity.window, view).isAppearanceLightStatusBars = !darkTheme
+        }
+    }
+
     MaterialTheme(
         colors = if (darkTheme) getDarkColorScheme() else getLightColorScheme(),
         typography = Typography(),
     ) {
         Surface(
-            modifier = modifier,
+            modifier = modifier.safeDrawingPadding(),
             color = MaterialTheme.colors.background,
             content = content,
         )

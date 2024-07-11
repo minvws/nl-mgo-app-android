f4efa32048f5a83ca193ad109bfcebefe2dc20e0
Bart Nijland <bartnijland91@gmail.com>
2024-07-11 13:29:37 +0200

    Fix MainViewModelTest

--- app/src/test/java/nl/rijksoverheid/mgo/MainViewModelTest.kt
+++ app/src/test/java/nl/rijksoverheid/mgo/MainViewModelTest.kt
@@ -1,9 +1,11 @@
 package nl.rijksoverheid.mgo
 
 import app.cash.turbine.test
+import io.mockk.mockk
 import nl.rijksoverheid.mgo.data.config.ConfigState
 import nl.rijksoverheid.mgo.data.config.TestConfigRepository
 import nl.rijksoverheid.mgo.data.onboarding.TestHasSeenOnboarding
+import nl.rijksoverheid.mgo.devicerooted.ShowDeviceRootedDialog
 import nl.rijksoverheid.mgo.framework.test.rules.MainDispatcherRule
 import org.junit.Assert.assertEquals
 import org.junit.Rule
@@ -20,7 +22,12 @@ internal class MainViewModelTest {
         val configRepository = TestConfigRepository()
         val hasSeenOnboarding = TestHasSeenOnboarding()
         hasSeenOnboarding.set(true)
-        val viewModel = MainViewModel(hasSeenOnboarding = hasSeenOnboarding, configRepository = configRepository)
+        val viewModel =
+            MainViewModel(
+                hasSeenOnboarding = hasSeenOnboarding,
+                configRepository = configRepository,
+                showDeviceRootedDialog = mockk<ShowDeviceRootedDialog>(),
+            )
 
         // When
         val hasSeen = viewModel.hasSeenOnboarding()
@@ -35,7 +42,12 @@ internal class MainViewModelTest {
             // Given
             val configRepository = TestConfigRepository()
             val hasSeenOnboarding = TestHasSeenOnboarding()
-            val viewModel = MainViewModel(hasSeenOnboarding = hasSeenOnboarding, configRepository = configRepository)
+            val viewModel =
+                MainViewModel(
+                    hasSeenOnboarding = hasSeenOnboarding,
+                    configRepository = configRepository,
+                    showDeviceRootedDialog = mockk<ShowDeviceRootedDialog>(),
+                )
 
             // When
             configRepository.setConfigState(ConfigState.UpdateRequired)

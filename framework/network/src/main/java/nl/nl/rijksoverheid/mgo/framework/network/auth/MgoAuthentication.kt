89e4893ec6fb9d773dbdcdb134060bd5063ce134
Bart Nijland <bartnijland91@gmail.com>
2024-07-29 14:31:01 +0200

    Formatting

--- framework/network/src/main/java/nl/nl/rijksoverheid/mgo/framework/network/auth/MgoAuthentication.kt
+++ framework/network/src/main/java/nl/nl/rijksoverheid/mgo/framework/network/auth/MgoAuthentication.kt
@@ -2,5 +2,6 @@ package nl.nl.rijksoverheid.mgo.framework.network.auth
 
 sealed class MgoAuthentication {
     data object None : MgoAuthentication()
+
     data class Basic(val user: String, val password: String) : MgoAuthentication()
 }

ebf512250f1affbb6dfb8e8fd7684a59f2b4a785
Bart Nijland <bartnijland91@gmail.com>
2024-09-26 13:47:03 +0200

    Rewrite test for list item

--- feature/dashboard/health_category/src/test/java/nl/rijksoverheid/mgo/feature/dashboard/healthCategory/HealthCategoryScreenViewModelTest.kt
+++ feature/dashboard/health_category/src/test/java/nl/rijksoverheid/mgo/feature/dashboard/healthCategory/HealthCategoryScreenViewModelTest.kt
@@ -4,8 +4,10 @@ import app.cash.turbine.test
 import nl.rijksoverheid.mgo.data.healthcare.HealthCareCategory
 import nl.rijksoverheid.mgo.data.healthcare.TEST_HEALTH_CARE_DATA_STATE_ERROR
 import nl.rijksoverheid.mgo.data.healthcare.TEST_HEALTH_CARE_DATA_STATE_LOADED
-import nl.rijksoverheid.mgo.data.healthcare.TestHealthCareStateRepository
+import nl.rijksoverheid.mgo.data.healthcare.TestHealthCareDataStatesRepository
+import nl.rijksoverheid.mgo.data.localisation.models.TEST_MGO_ORGANIZATION
 import nl.rijksoverheid.mgo.framework.test.rules.MainDispatcherRule
+import nl.rijksoverheid.mgo.localisation.TestOrganizationRepository
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Rule
@@ -20,13 +22,15 @@ internal class HealthCategoryScreenViewModelTest {
     fun `Given medications, When creating viewmodel, List items are shown`() =
         runTest {
             // Given
-            val healthCareStateRepository = TestHealthCareStateRepository(listOf(TEST_HEALTH_CARE_DATA_STATE_LOADED))
+            val healthCareDataStatesRepository = TestHealthCareDataStatesRepository(listOf(TEST_HEALTH_CARE_DATA_STATE_LOADED))
+            val organizationRepository = TestOrganizationRepository()
 
             // When
             val viewModel =
                 HealthCategoryScreenViewModel(
                     arguments = HealthCategoryScreenArguments(category = HealthCareCategory.MEDICATIONS, filterOrganization = null),
-                    healthCareStateRepository = healthCareStateRepository,
+                    healthCareDataStatesRepository = healthCareDataStatesRepository,
+                    organizationRepository = organizationRepository,
                 )
 
             // Then
@@ -37,15 +41,53 @@ internal class HealthCategoryScreenViewModelTest {
         }
 
     @Test
-    fun `Given failed medications, When calling retry, Show error banner is updated`() =
+    fun `Given failed medications for organization, When calling retry, Show error banner is updated`() =
         runTest {
             // Given
-            val healthCareStateRepository = TestHealthCareStateRepository(listOf(TEST_HEALTH_CARE_DATA_STATE_ERROR))
-            healthCareStateRepository.setRefreshData(listOf(TEST_HEALTH_CARE_DATA_STATE_LOADED))
+            val healthCareDataStatesRepository = TestHealthCareDataStatesRepository(listOf(TEST_HEALTH_CARE_DATA_STATE_ERROR))
+            healthCareDataStatesRepository.setRefreshData(listOf(TEST_HEALTH_CARE_DATA_STATE_LOADED))
+            val organizationRepository = TestOrganizationRepository()
+
+            // When
+            val viewModel =
+                HealthCategoryScreenViewModel(
+                    arguments =
+                        HealthCategoryScreenArguments(
+                            category = HealthCareCategory.MEDICATIONS,
+                            filterOrganization = TEST_MGO_ORGANIZATION,
+                        ),
+                    healthCareDataStatesRepository = healthCareDataStatesRepository,
+                    organizationRepository = organizationRepository,
+                )
+
+            // When
+            viewModel.viewState.test {
+                val emit1 = awaitItem()
+                assertTrue(emit1.showErrorBanner)
+
+                viewModel.retry()
+
+                // Then
+                val emit2 = awaitItem()
+                assertFalse(emit2.showErrorBanner)
+            }
+        }
+
+    @Test
+    fun `Given failed medications for category, When calling retry, Show error banner is updated`() =
+        runTest {
+            // Given
+            val healthCareDataStatesRepository = TestHealthCareDataStatesRepository(listOf(TEST_HEALTH_CARE_DATA_STATE_ERROR))
+            healthCareDataStatesRepository.setRefreshData(listOf(TEST_HEALTH_CARE_DATA_STATE_LOADED))
+            val organizationRepository = TestOrganizationRepository()
+            organizationRepository.setStoredProviders(listOf(TEST_MGO_ORGANIZATION))
+
+            // When
             val viewModel =
                 HealthCategoryScreenViewModel(
                     arguments = HealthCategoryScreenArguments(category = HealthCareCategory.MEDICATIONS, filterOrganization = null),
-                    healthCareStateRepository = healthCareStateRepository,
+                    healthCareDataStatesRepository = healthCareDataStatesRepository,
+                    organizationRepository = organizationRepository,
                 )
 
             // When

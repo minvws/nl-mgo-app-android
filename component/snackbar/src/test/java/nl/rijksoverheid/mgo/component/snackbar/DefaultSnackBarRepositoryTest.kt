abf140ff6d8e01f73310c75157aba173ec0f6641
Bart Nijland <bartnijland91@gmail.com>
2024-10-03 09:19:39 +0200

    Write test for DefaultSnackBarRepository

---
+++ component/snackbar/src/test/java/nl/rijksoverheid/mgo/component/snackbar/DefaultSnackBarRepositoryTest.kt
@@ -0,0 +1,41 @@
+package nl.rijksoverheid.mgo.component.snackbar
+
+import app.cash.turbine.test
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import kotlinx.coroutines.test.runTest
+
+internal class DefaultSnackBarRepositoryTest {
+    @Test
+    fun `Given repository, When calling show, The visuals are emitted`() =
+        runTest {
+            // Given
+            val repository = DefaultSnackBarRepository()
+            val visuals = MgoSnackBarVisuals(type = MgoSnackBarType.SUCCESS, title = -1)
+
+            // When
+            repository.show(visuals)
+
+            // Then
+            repository.get().test {
+                assertEquals(visuals, awaitItem())
+            }
+        }
+
+    @Test
+    fun `Given repository, When calling dismiss, The visuals are no longer emitted`() =
+        runTest {
+            // Given
+            val repository = DefaultSnackBarRepository()
+            val visuals = MgoSnackBarVisuals(type = MgoSnackBarType.SUCCESS, title = -1)
+
+            // When
+            repository.show(visuals)
+            repository.dismiss()
+
+            // Then
+            repository.get().test {
+                expectNoEvents()
+            }
+        }
+}

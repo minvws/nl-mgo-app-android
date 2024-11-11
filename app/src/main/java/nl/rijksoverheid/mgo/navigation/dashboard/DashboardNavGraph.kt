8b2fc74615f54b85e758c3b4a564c34d40087cd0
Bart Nijland <bartnijland91@gmail.com>
2024-11-11 14:38:02 +0100

    Re-add dummy about this app page

--- app/src/main/java/nl/rijksoverheid/mgo/navigation/dashboard/DashboardNavGraph.kt
+++ app/src/main/java/nl/rijksoverheid/mgo/navigation/dashboard/DashboardNavGraph.kt
@@ -1,14 +1,19 @@
 package nl.rijksoverheid.mgo.navigation.dashboard
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import nl.rijksoverheid.mgo.component.theme.headingLarge
 import nl.rijksoverheid.mgo.feature.dashboard.bottombar.DashboardBottomBarScreen
 
 fun NavGraphBuilder.addDashboardNavGraph(rootNavController: NavController) {
@@ -26,12 +31,27 @@ fun NavGraphBuilder.addDashboardNavGraph(rootNavController: NavController) {
                 aboutThisAppStartDestination = DashboardNavigation.AboutThisApp.Root,
                 aboutThisAppNavGraph = { navController ->
                     composable<DashboardNavigation.AboutThisApp.Root> {
-                        Box(
-                            modifier =
-                                Modifier
-                                    .fillMaxSize()
-                                    .background(Color.Red),
-                        )
+                        val applicationContext = LocalContext.current.applicationContext
+                        Scaffold { innerPadding ->
+                            Column(modifier = Modifier.padding(innerPadding).padding(horizontal = 16.dp)) {
+                                Text(
+                                    modifier = Modifier.padding(top = 32.dp),
+                                    text = "Over de app",
+                                    style = MaterialTheme.typography.headingLarge,
+                                )
+                                Button(
+                                    modifier = Modifier.padding(top = 16.dp),
+                                    onClick = {
+                                        val packageName = applicationContext.packageName
+                                        val runtime = Runtime.getRuntime()
+                                        runtime.exec("pm clear $packageName")
+                                    },
+                                    content = {
+                                        Text(text = "Reset App")
+                                    },
+                                )
+                            }
+                        }
                     }
                 },
             )

9c6f27bfa54475211debb0abad1a4c48a0cd010c
Bart Nijland <bartnijland91@gmail.com>
2024-11-28 11:42:43 +0100

    Use @binds in environment module

--- framework/environment/src/main/java/nl/rijksoverheid/mgo/framework/environment/EnvironmentModule.kt
+++ framework/environment/src/main/java/nl/rijksoverheid/mgo/framework/environment/EnvironmentModule.kt
@@ -1,21 +1,15 @@
 package nl.rijksoverheid.mgo.framework.environment
 
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Named
 import javax.inject.Singleton
 
-@InstallIn(SingletonComponent::class)
 @Module
-object EnvironmentModule {
-    @Provides
+@InstallIn(SingletonComponent::class)
+internal abstract class EnvironmentModule {
+    @Binds
     @Singleton
-    fun provideEnvironmentRepository(
-        @Named("appFlavor") appFlavor: String,
-        @Named("versionCode") versionCode: Int,
-    ): EnvironmentRepository {
-        return DefaultEnvironmentRepository(appFlavor = appFlavor, versionCode = versionCode)
-    }
+    abstract fun provideEnvironmentRepository(default: DefaultEnvironmentRepository): EnvironmentRepository
 }

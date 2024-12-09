0555d3d9a7f98516ae9561010899609c1d46a049
Bart Nijland <bartnijland91@gmail.com>
2024-12-09 16:23:25 +0100

    Create DigidMockScreen

---
+++ feature/digid/src/main/java/nl/rijksoverheid/mgo/feature/digid/DigidMockScreen.kt
@@ -0,0 +1,61 @@
+package nl.rijksoverheid.mgo.feature.digid
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import nl.rijksoverheid.mgo.component.theme.DefaultPreviews
+import nl.rijksoverheid.mgo.component.theme.MgoTheme
+import nl.rijksoverheid.mgo.component.theme.composable.MgoScaffold
+import nl.rijksoverheid.mgo.component.theme.headingLarge
+import nl.rijksoverheid.mgo.framework.copy.R as CopyR
+
+@Composable
+fun DigidMockScreen(onNavigateToAutomaticLocalisation: () -> Unit) {
+    MgoScaffold(
+        primaryButtonText = stringResource(id = CopyR.string.common_next),
+        onPrimaryButtonClick = onNavigateToAutomaticLocalisation,
+    ) {
+        Image(
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .padding(top = TopAppBarDefaults.MediumAppBarCollapsedHeight)
+                    .align(Alignment.CenterHorizontally),
+            painter = painterResource(id = R.drawable.illustration_mock),
+            contentDescription = null,
+        )
+
+        Text(
+            modifier = Modifier.padding(top = 32.dp),
+            text = stringResource(id = CopyR.string.login_info_heading),
+            style = MaterialTheme.typography.headingLarge,
+            fontWeight = FontWeight.Bold,
+        )
+
+        Text(
+            modifier = Modifier.padding(top = 16.dp),
+            text = stringResource(id = CopyR.string.login_info_subheading),
+            style = MaterialTheme.typography.bodySmall,
+        )
+    }
+}
+
+@DefaultPreviews
+@Composable
+internal fun DigidMockScreenPreview() {
+    MgoTheme {
+        DigidMockScreen(
+            onNavigateToAutomaticLocalisation = {},
+        )
+    }
+}

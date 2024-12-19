1657fbd29a6e2b765cc7e74d81ab71b61a2e518c
Bart Nijland <bartnijland91@gmail.com>
2024-12-19 15:05:36 +0100

    Fix Paparazzi bug

--- build-logic/plugins/src/main/java/AndroidFeaturePlugin.kt
+++ build-logic/plugins/src/main/java/AndroidFeaturePlugin.kt
@@ -14,6 +14,9 @@ class AndroidFeaturePlugin : Plugin<Project> {
             apply(AndroidConventionsPlugin::class.java)
             apply(AndroidUiPlugin::class.java)
             apply(LintPlugin::class.java)
+
+            // Bug in Paparazzi 1.3.5, see: https://github.com/cashapp/paparazzi/issues/1692#issuecomment-2489002011
+            CreateSnapshotsDirTask.register(project)
             apply(SnapshotsPlugin::class.java)
         }
     }

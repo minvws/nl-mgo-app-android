c0427f79e4b7474a205dfa475c69c75c8fdd49c7
Bart Nijland <bartnijland91@gmail.com>
2025-01-15 14:35:49 +0100

    Fix UiSchemaDetailScreenViewModelTest

--- feature/dashboard/uischema_detail/build.gradle.kts
+++ feature/dashboard/uischema_detail/build.gradle.kts
@@ -10,4 +10,5 @@ dependencies {
     implementation(projects.data.healthcare)
     implementation(projects.data.localisation)
     testImplementation(testFixtures(projects.data.healthcare))
+    testImplementation(testFixtures(projects.data.fhirParser))
 }

0bfecc6084d77669a0084c91a5b56724f0ec66b0
Bart Nijland <bartnijland91@gmail.com>
2025-02-12 11:10:13 +0100

    Fix tests

--- data/fhirParser/src/testFixtures/java/nl/rijksoverheid/mgo/data/fhirParser/uiSchema/TestUiSchemaMapper.kt
+++ data/fhirParser/src/testFixtures/java/nl/rijksoverheid/mgo/data/fhirParser/uiSchema/TestUiSchemaMapper.kt
@@ -1,26 +1,26 @@
 package nl.rijksoverheid.mgo.data.fhirParser.uiSchema
 
 import nl.rijksoverheid.mgo.data.fhirParser.mgoResource.MgoResource
+import nl.rijksoverheid.mgo.data.fhirParser.models.UiSchema
 import nl.rijksoverheid.mgo.data.fhirParser.shared.TEST_UI_SCHEMA
-import nl.rijksoverheid.mgo.data.fhirParser.shared.UISchema
 
 class TestUiSchemaMapper : UiSchemaMapper {
-    private var summary: UISchema = TEST_UI_SCHEMA
-    private var detail: UISchema = TEST_UI_SCHEMA
+    private var summary: UiSchema = TEST_UI_SCHEMA
+    private var detail: UiSchema = TEST_UI_SCHEMA
 
-    fun setSummary(uiSchema: UISchema) {
+    fun setSummary(uiSchema: UiSchema) {
         this.summary = uiSchema
     }
 
-    fun setDetail(uiSchema: UISchema) {
+    fun setDetail(uiSchema: UiSchema) {
         this.detail = uiSchema
     }
 
-    override suspend fun getSummary(mgoResource: MgoResource): UISchema {
+    override suspend fun getSummary(mgoResource: MgoResource): UiSchema {
         return summary
     }
 
-    override suspend fun getDetail(mgoResource: MgoResource): UISchema {
+    override suspend fun getDetail(mgoResource: MgoResource): UiSchema {
         return detail
     }
 }

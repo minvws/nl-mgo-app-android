714f0f05448030f217876859c746cfe2c791e3f5
Bart Nijland <bartnijland91@gmail.com>
2025-03-04 15:42:36 +0100

    Document :framework:util

--- framework/util/src/main/java/nl/rijksoverheid/mgo/framework/util/OpenBrowser.kt
+++ framework/util/src/main/java/nl/rijksoverheid/mgo/framework/util/OpenBrowser.kt
@@ -7,16 +7,23 @@ import android.net.Uri
 import androidx.browser.customtabs.CustomTabsIntent
 
 /**
- * Launches the url in Chrome Custom Tabs (https://developer.chrome.com/docs/android/custom-tabs).
- * If it fails, will fallback to browser.
+ * Opens a URL in a web browser using Chrome Custom Tabs if available,
+ * falling back to the default browser if necessary.
+ *
+ * This function attempts to launch the given URL using Chrome Custom Tabs for
+ * an optimized browsing experience. If Chrome is not available or disabled,
+ * it falls back to a standard web browser intent.
+ *
+ * @param url The web address to open.
  */
 fun Context.launchBrowser(url: String) {
     try {
+        // Attempt to open the URL using Chrome Custom Tabs
         CustomTabsIntent.Builder().build().also { intent ->
             intent.launchUrl(this, Uri.parse(url))
         }
     } catch (exception: ActivityNotFoundException) {
-        // if chrome app is disabled or not there, try an alternative
+        // If Chrome is unavailable, fall back to a regular web browser
         startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
     }
 }

d7a665ddab1258e590cd4a61f16cf4ff4ac7ea48
Bart Nijland <bartnijland91@gmail.com>
2025-03-12 14:00:10 +0100

    Adjust requests to match new backend rules

--- data/fhirParser/src/main/java/nl/rijksoverheid/mgo/data/fhirParser/mgoResource/FhirVersion.kt
+++ data/fhirParser/src/main/java/nl/rijksoverheid/mgo/data/fhirParser/mgoResource/FhirVersion.kt
@@ -3,7 +3,7 @@ package nl.rijksoverheid.mgo.data.fhirParser.mgoResource
 /**
  * Represents FHIR version (https://hl7.org/fhir/).
  */
-enum class FhirVersion {
-    R3,
-    R4,
+enum class FhirVersion(val versionNumber: String) {
+    R3("3.0"),
+    R4("4.0"),
 }

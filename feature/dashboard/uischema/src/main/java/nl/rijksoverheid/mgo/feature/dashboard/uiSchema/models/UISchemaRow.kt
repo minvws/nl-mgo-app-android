9f900d61090a88c9474b9b1382d0fe23b5a474fa
Bart Nijland <bartnijland91@gmail.com>
2025-04-03 11:21:58 +0200

    Handle DOWNLOAD_LINK in ui schema

--- feature/dashboard/uischema/src/main/java/nl/rijksoverheid/mgo/feature/dashboard/uiSchema/models/UISchemaRow.kt
+++ feature/dashboard/uischema/src/main/java/nl/rijksoverheid/mgo/feature/dashboard/uiSchema/models/UISchemaRow.kt
@@ -39,12 +39,12 @@ internal sealed class UISchemaRow(open val heading: String?, open val value: Str
     data class Reference(override val heading: String?, override val value: String, val referenceId: String) : UISchemaRow(heading, value)
 
     /**
-     * Represents a clickable list item that can download and open a file.
+     * Represents a clickable list item that can download a file from a FHIR binary resource.
      *
      * @param heading The top text of the list item.
      * @param value The bottom text of the list item.
      */
-    sealed class File(override val heading: String?, override val value: String) : UISchemaRow(heading, value) {
+    sealed class Binary(override val heading: String?, override val value: String) : UISchemaRow(heading, value) {
         /**
          * Represents the state that a file has not been downloaded (yet).
          *
@@ -52,7 +52,7 @@ internal sealed class UISchemaRow(open val heading: String?, open val value: Str
          * @param value The bottom text of the list item.
          * @param binary A string to use in [FhirBinaryRepository] to get the binary.
          */
-        sealed class NotDownloaded(override val heading: String?, override val value: String, open val binary: String) : File(
+        sealed class NotDownloaded(override val heading: String?, override val value: String, open val binary: String) : Binary(
             heading,
             value,
         ) {
@@ -83,7 +83,7 @@ internal sealed class UISchemaRow(open val heading: String?, open val value: Str
          * @param heading The top text of the list item.
          * @param value The bottom text of the list item.
          */
-        data class Loading(override val heading: String?, override val value: String) : File(heading, value)
+        data class Loading(override val heading: String?, override val value: String) : Binary(heading, value)
 
         /**
          * Represents the state that the list item should have a file, but it does not exist.
@@ -91,7 +91,7 @@ internal sealed class UISchemaRow(open val heading: String?, open val value: Str
          * @param heading The top text of the list item.
          * @param value The bottom text of the list item.
          */
-        data class Empty(override val heading: String?, override val value: String) : File(heading, value)
+        data class Empty(override val heading: String?, override val value: String) : Binary(heading, value)
 
         /**
          * Represents the state that the list item has downloaded the file. Clicking it should open the file.
@@ -100,8 +100,17 @@ internal sealed class UISchemaRow(open val heading: String?, open val value: Str
          * @param value The bottom text of the list item.
          * @param binary The [FhirBinary] containing the downloaded file.
          */
-        data class Downloaded(override val heading: String?, override val value: String, val binary: FhirBinary) : File(heading, value)
+        data class Downloaded(override val heading: String?, override val value: String, val binary: FhirBinary) : Binary(heading, value)
     }
+
+    /**
+     * Represents a clickable list item that can download a file from a link.
+     *
+     * @param heading The top text of the list item.
+     * @param value The bottom text of the list item.
+     * @param url The url to download the file.
+     */
+    data class Link(override val heading: String?, override val value: String, val url: String) : UISchemaRow(heading, value)
 }
 
 internal fun UiElement.toRow(): UISchemaRow {
@@ -111,12 +120,7 @@ internal fun UiElement.toRow(): UISchemaRow {
         }
 
         is DownloadLink -> {
-            val url = this.url
-            if (url == null) {
-                UISchemaRow.File.Empty(heading = null, value = this.label)
-            } else {
-                UISchemaRow.File.NotDownloaded.Idle(heading = null, value = this.label, binary = url)
-            }
+            UISchemaRow.Link(heading = null, value = this.label, this.url ?: "")
         }
 
         is SingleValue -> {
@@ -138,9 +142,9 @@ internal fun UiElement.toRow(): UISchemaRow {
         is DownloadBinary -> {
             val reference = this.reference
             if (reference == null) {
-                UISchemaRow.File.Empty(heading = null, value = this.label)
+                UISchemaRow.Binary.Empty(heading = null, value = this.label)
             } else {
-                UISchemaRow.File.NotDownloaded.Idle(heading = null, value = this.label, binary = reference)
+                UISchemaRow.Binary.NotDownloaded.Idle(heading = null, value = this.label, binary = reference)
             }
         }
     }

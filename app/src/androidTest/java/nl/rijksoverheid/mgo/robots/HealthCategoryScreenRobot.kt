2825249867bfae1a4528769407f3add3013f7927
Bart Nijland <bartnijland91@gmail.com>
2025-06-23 13:33:39 +0200

    Created NestedUiSchemaTest

--- app/src/androidTest/java/nl/rijksoverheid/mgo/robots/HealthCategoryScreenRobot.kt
+++ app/src/androidTest/java/nl/rijksoverheid/mgo/robots/HealthCategoryScreenRobot.kt
@@ -1,6 +1,9 @@
 package nl.rijksoverheid.mgo.robots
 
 import androidx.compose.ui.test.junit4.ComposeTestRule
+import androidx.compose.ui.test.onAllNodesWithTag
+import androidx.compose.ui.test.onFirst
+import androidx.compose.ui.test.performClick
 import nl.rijksoverheid.mgo.feature.dashboard.healthCategory.HealthCategoryScreenTestTag
 import nl.rijksoverheid.mgo.utils.assertListItems
 import nl.rijksoverheid.mgo.utils.waitForListItems
@@ -13,4 +16,12 @@ class HealthCategoryScreenRobot(
     composeTestRule.assertListItems(HealthCategoryScreenTestTag.CARD)
     return this
   }
+
+  fun clickFirstListItem(): HealthCategoryScreenRobot {
+    composeTestRule.waitForListItems(HealthCategoryScreenTestTag.CARD)
+    composeTestRule.onAllNodesWithTag(HealthCategoryScreenTestTag.CARD).onFirst().performClick()
+    return this
+  }
+
+  fun gotoUiSchemaScreen(): UiSchemaScreenRobot = UiSchemaScreenRobot(composeTestRule)
 }

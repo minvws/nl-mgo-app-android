257c41792f8ec58b5669e401393cc784217bc97f
Bart Nijland <bartnijland91@gmail.com>
2025-08-27 16:36:42 +0200

    Favorite empty card in EditOverviewBottomSheet

--- feature/dashboard/edit_overview/src/main/java/nl/rijksoverheid/mgo/feature/dashboard/editOverview/EditOverviewBottomSheet.kt
+++ feature/dashboard/edit_overview/src/main/java/nl/rijksoverheid/mgo/feature/dashboard/editOverview/EditOverviewBottomSheet.kt
@@ -2,11 +2,14 @@ package nl.rijksoverheid.mgo.feature.dashboard.editOverview
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.BottomSheetDefaults
@@ -24,12 +27,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
+import nl.rijksoverheid.mgo.component.mgo.MgoCard
 import nl.rijksoverheid.mgo.component.mgo.MgoTopAppBar
 import nl.rijksoverheid.mgo.component.theme.DefaultPreviews
 import nl.rijksoverheid.mgo.component.theme.MgoTheme
+import nl.rijksoverheid.mgo.component.theme.contentSecondary
 import nl.rijksoverheid.mgo.component.theme.supportRijkslint
 import nl.rijksoverheid.mgo.data.healthcare.mgoResource.category.HealthCareCategory
+import nl.rijksoverheid.mgo.data.healthcare.mgoResource.category.HealthCareCategoryId
 import nl.rijksoverheid.mgo.data.healthcare.mgoResource.category.TEST_HEALTH_CARE_CATEGORIES
 import nl.rijksoverheid.mgo.framework.copy.R as CopyR
 
@@ -81,7 +88,27 @@ private fun EditOverviewBottomSheetContent(
       )
     },
   ) { innerPadding ->
-    Box(modifier = Modifier.fillMaxSize().background(Color.Red).padding(innerPadding))
+    LazyColumn(modifier = Modifier.padding(innerPadding), contentPadding = PaddingValues(horizontal = 16.dp)) {
+      item {
+        Text(text = stringResource(CopyR.string.edit_overview_favorites_heading), style = MaterialTheme.typography.headlineSmall)
+        FavoriteCard(modifier = Modifier.padding(top = 8.dp), favorites = listOf())
+      }
+    }
+  }
+}
+
+@Composable
+private fun FavoriteCard(
+  modifier: Modifier = Modifier,
+  favorites: List<HealthCareCategory>,
+) {
+  MgoCard(modifier = modifier) {
+    Text(
+      modifier = Modifier.padding(16.dp),
+      text = stringResource(CopyR.string.edit_overview_favorites_empty),
+      color = MaterialTheme.colorScheme.contentSecondary(),
+      style = MaterialTheme.typography.bodyMedium,
+    )
   }
 }
 

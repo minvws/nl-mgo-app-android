078dedae91bed3efb6190d18b7201add4fd2503a
Bart Nijland <bartnijland91@gmail.com>
2025-08-27 16:13:02 +0200

    Top app bar for EditOverviewBottomScreen

--- feature/dashboard/edit_overview/src/main/java/nl/rijksoverheid/mgo/feature/dashboard/editOverview/EditOverviewBottomSheet.kt
+++ feature/dashboard/edit_overview/src/main/java/nl/rijksoverheid/mgo/feature/dashboard/editOverview/EditOverviewBottomSheet.kt
@@ -7,19 +7,35 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import kotlinx.coroutines.launch
+import nl.rijksoverheid.mgo.component.mgo.MgoTopAppBar
 import nl.rijksoverheid.mgo.component.theme.DefaultPreviews
 import nl.rijksoverheid.mgo.component.theme.MgoTheme
+import nl.rijksoverheid.mgo.component.theme.supportRijkslint
+import nl.rijksoverheid.mgo.data.healthcare.mgoResource.category.HealthCareCategory
+import nl.rijksoverheid.mgo.data.healthcare.mgoResource.category.TEST_HEALTH_CARE_CATEGORIES
+import nl.rijksoverheid.mgo.framework.copy.R as CopyR
 
 @Composable
 fun EditOverviewBottomSheet(onDismissRequest: () -> Unit) {
+  val coroutineScope = rememberCoroutineScope()
   val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
 
   ModalBottomSheet(
@@ -28,13 +44,43 @@ fun EditOverviewBottomSheet(onDismissRequest: () -> Unit) {
     sheetState = sheetState,
     dragHandle = { BottomSheetDefaults.DragHandle() },
   ) {
-    EditOverviewBottomSheetContent()
+    EditOverviewBottomSheetContent(
+      onSave = { categories ->
+      },
+      onNavigateBack = {
+        coroutineScope.launch {
+          sheetState.hide()
+          onDismissRequest()
+        }
+      },
+    )
   }
 }
 
 @Composable
-private fun EditOverviewBottomSheetContent() {
-  Scaffold(modifier = Modifier.fillMaxWidth().fillMaxHeight(0.95f)) { innerPadding ->
+private fun EditOverviewBottomSheetContent(
+  onSave: (categories: List<HealthCareCategory>) -> Unit,
+  onNavigateBack: () -> Unit,
+) {
+  Scaffold(
+    modifier = Modifier.fillMaxWidth().fillMaxHeight(0.95f),
+    topBar = {
+      MgoTopAppBar(
+        title = stringResource(CopyR.string.edit_overview_heading),
+        onNavigateBack = onNavigateBack,
+        windowInsets = WindowInsets(0),
+        navigationIcon = Icons.Default.Close,
+        actions = {
+          TextButton(
+            { onSave(TEST_HEALTH_CARE_CATEGORIES) },
+            colors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colorScheme.supportRijkslint()),
+          ) {
+            Text(text = stringResource(CopyR.string.edit_overview_save), style = MaterialTheme.typography.bodyMedium, fontWeight = FontWeight.Bold)
+          }
+        },
+      )
+    },
+  ) { innerPadding ->
     Box(modifier = Modifier.fillMaxSize().background(Color.Red).padding(innerPadding))
   }
 }
@@ -43,6 +89,9 @@ private fun EditOverviewBottomSheetContent() {
 @Composable
 private fun EditOverviewBottomSheetPreview() {
   MgoTheme {
-    EditOverviewBottomSheetContent()
+    EditOverviewBottomSheetContent(
+      onSave = {},
+      onNavigateBack = {},
+    )
   }
 }

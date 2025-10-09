660473f7a5c70ec9c945c8638119f6012477d8ff
Bart Nijland <bartnijland91@gmail.com>
2025-10-09 13:17:18 +0200

    Write test for HealthCategoriesListItemViewModel

---
+++ feature/dashboard/health_categories/src/test/java/nl/rijksoverheid/mgo/feature/dashboard/healthCategories/listItem/HealthCategoriesFavoriteCardViewModelTest.kt
@@ -0,0 +1,53 @@
+package nl.rijksoverheid.mgo.feature.dashboard.healthCategories.listItem
+
+import app.cash.turbine.test
+import kotlinx.coroutines.test.runTest
+import nl.rijksoverheid.mgo.data.fhir.TEST_FHIR_RESPONSE_SUCCESS
+import nl.rijksoverheid.mgo.data.fhir.TestFhirRepository
+import nl.rijksoverheid.mgo.data.healthCategories.GetEndpointsForHealthCategory
+import nl.rijksoverheid.mgo.data.healthCategories.JvmGetDataSetsFromDisk
+import nl.rijksoverheid.mgo.data.healthCategories.models.HealthCategoryGroup
+import nl.rijksoverheid.mgo.data.healthCategories.models.TEST_HEALTH_CATEGORY_PROBLEMS
+import nl.rijksoverheid.mgo.data.localisation.models.TEST_MGO_ORGANIZATION
+import nl.rijksoverheid.mgo.framework.test.rules.MainDispatcherRule
+import nl.rijksoverheid.mgo.localisation.TestOrganizationRepository
+import org.junit.Assert.assertFalse
+import org.junit.Rule
+import org.junit.Test
+
+class HealthCategoriesFavoriteCardViewModelTest {
+  @get:Rule
+  val mainDispatcherRule = MainDispatcherRule()
+
+  private val organizationRepository = TestOrganizationRepository()
+  private val getDataSetsFromDisk = JvmGetDataSetsFromDisk()
+  private val getEndpointsForHealthCategory = GetEndpointsForHealthCategory(getDataSetsFromDisk)
+  private val fhirRepository = TestFhirRepository()
+
+  @Test
+  fun testLoaded() =
+    runTest {
+      // Given: stored organization
+      organizationRepository.setStoredProviders(listOf(TEST_MGO_ORGANIZATION))
+
+      // Given: Fhir repository returns success that is non empty
+      fhirRepository.setObserveResult(TEST_FHIR_RESPONSE_SUCCESS(isEmpty = false))
+
+      // When: Creating viewmodel and not filtering on organization
+      val viewModel = createViewModel(category = TEST_HEALTH_CATEGORY_PROBLEMS)
+
+      // Then: List item state is loaded
+      viewModel.isLoading.test {
+        assertFalse(awaitItem())
+      }
+    }
+
+  private fun createViewModel(category: HealthCategoryGroup.HealthCategory) =
+    HealthCategoriesFavoriteCardViewModel(
+      category = category,
+      getEndpointsForHealthCategory = getEndpointsForHealthCategory,
+      organizationRepository = organizationRepository,
+      fhirRepository = fhirRepository,
+      ioDispatcher = mainDispatcherRule.testDispatcher,
+    )
+}

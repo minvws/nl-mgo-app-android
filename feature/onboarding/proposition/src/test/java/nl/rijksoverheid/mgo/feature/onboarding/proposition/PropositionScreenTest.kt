aaa9b0b22339cb82a75707b2331e2b4b2a318733
Bart Nijland <bartnijland91@gmail.com>
2026-02-03 12:58:31 +0100

    Update privacy screen design

--- feature/onboarding/proposition/src/test/java/nl/rijksoverheid/mgo/feature/onboarding/proposition/PropositionScreenTest.kt
+++ feature/onboarding/proposition/src/test/java/nl/rijksoverheid/mgo/feature/onboarding/proposition/PropositionScreenTest.kt
@@ -4,7 +4,11 @@ import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.click
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTouchInput
+import nl.rijksoverheid.mgo.component.mgo.MgoBottomButtons
+import nl.rijksoverheid.mgo.component.mgo.MgoBottomButtonsTestTag
 import nl.rijksoverheid.mgo.component.theme.MgoTheme
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
@@ -37,8 +41,8 @@ internal class PropositionScreenTest {
     }
 
     composeTestRule
-      .onNode(hasText("privacyverklaring", substring = true))
-      .performTouchInput { click(percentOffset(0.1f, 0f)) }
+      .onNodeWithTag(MgoBottomButtonsTestTag.SECONDARY_BUTTON)
+      .performClick()
 
     val shadowActivity = Shadows.shadowOf(activity)
     val startedIntent = shadowActivity.nextStartedActivity
